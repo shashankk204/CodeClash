4f82c6386c3d02d949eba7c71225f3325e72204c
Shashank kumar <shashankk204@gmail.com>
2025-01-16 14:10:31 +0530

    fixed some minor bugs

--- src/components/post/editor/mutations.tsx
+++ src/components/post/editor/mutations.tsx
@@ -8,16 +8,31 @@ import {
 } from "@tanstack/react-query";
 import { submitPost } from "./action";
 import { useToast } from "@/hooks/use-toast";
+import { useSession } from "next-auth/react";
+import { Loader2 } from "lucide-react";
 
 export function useSubmitPostMutation() {
     const { toast } = useToast();
+
+    const session=useSession();
+
+    if(session.status==='loading') return <Loader2 className="mx-auto my-3 animate-spin" />
 
     const queryClient = useQueryClient();
 
     const mutation = useMutation({
         mutationFn: submitPost,
         onSuccess: async (newPost) => {
-            const queryFilter: QueryFilters = { queryKey: ["post-feed", "for-you"] };
+            const queryFilter = { queryKey: ["post-feed"],
+                predicate(query)
+                {
+                    return query.queryKey.includes("for-you")||
+                    (query.queryKey.includes('user-posts') &&
+                    query.queryKey.includes(session.data?.user?.id)
+                    )
+                }
+
+             }satisfies QueryFilters ;
             await queryClient.cancelQueries(queryFilter);
 
             queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
@@ -44,7 +59,7 @@ export function useSubmitPostMutation() {
             queryClient.invalidateQueries({
                 queryKey: queryFilter.queryKey,
                 predicate(query) {
-                    return !query.state.data;
+                    return queryFilter.predicate(query) && !query.state.data;
                 },
             });
 
